74ecabd93b1e22c69053e6848c14c31eed4332cc
Emily Nelson <evnelson2021@gmail.com>
2023-01-02 17:13:59 -0600

    last edits for 1/2/23

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -6,9 +6,19 @@ export const Home = () => {
         <Routes>
             <Route path="*" element={
                 <>
-                    <h1 className="future-page">Future Landing Page of Over the Rainbow</h1>
-                    <h2 className="backstory">Over the Rainbow is a co-parenitng site develped for families that deal with the intricacies of a split household on a daily basis.</h2>
+                    <h1 className="future-page">Unlock the power of living somewhere over the rainbow</h1>
+                    <h2 className="backstory">Over the Rainbow is a co-parenting site developed in 2022 for families that deal with the intricacies of a split household on a daily basis.</h2>
+                
+                <h1 className="future-page">Let us help you:</h1>
+
                     <div className="work-in-progress"></div>
+
+                <div className="home-grid">
+                    <div className="home-thumbnail-1"></div>
+                    <div className="home-thumbnail-2"></div>
+                    <div className="home-thumbnail-3"></div>
+                    <div className="home-thumbnail-4"></div>
+                </div>
 
                     <Outlet />
                 </>
